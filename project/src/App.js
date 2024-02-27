0237221febe4c77fe0946ec506630977b35f8952
vishal kumar <vishalkumar.itcs@gmail.com>
2024-02-28 01:08:10 +0530

    update

---
+++ project/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import "bootstrap/dist/css/bootstrap.css"
+import "bootstrap/dist/js/bootstrap.bundle"
+
+function App() {
+  return (
+    <div className="main">
+    </div>
+  );
+}
+
+export default App;
