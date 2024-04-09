01cddc2abd1a5d9182276938003503349d4aa9fc
vishal kumar <vishalkumar.itcs@gmail.com>
2024-04-10 00:14:43 +0530

    commit

--- Weather-App/src/components/Credit.js
+++ Weather-App/src/components/Credit.js
@@ -4,10 +4,7 @@ export default function Credit() {
   return (
     <div class="credit">
     <p>
-      Coded by
-      <a href="https://www.shecodes.io/graduates/73033-chaimae-lamirine"
-        > Chaimae Lamirine</a
-      >, <a href="https://github.com/shemmee/React-Weather-App">Open sourced on Github.</a> Hosted on Netlify
+      Vishal Kumar
     </p>
   </div>
   );
